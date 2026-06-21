a1c36d7b0150c727df1d968cd14ee4c54bf001a3
billshin <billshin@auo.com>
2026-06-21 20:46:42 +0800

    add base

--- idea-graph-app/src/App.tsx
+++ idea-graph-app/src/App.tsx
@@ -5,7 +5,7 @@ import IdeaEditorPage from './pages/IdeaEditorPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<IdeaListPage />} />
         <Route path="/editor/:projectId" element={<IdeaEditorPage />} />
