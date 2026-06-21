7cd13bcc55ddf7055a15372544e23442d6943a95
billshin <billshin@auo.com>
2026-06-21 20:51:24 +0800

    add base with -

--- idea-graph-app/vite.config.ts
+++ idea-graph-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/IdeaExecutionGraph/',
+  base: '/Idea-Execution-Graph/',
   build: {
     outDir: '../docs',
     emptyOutDir: true,
